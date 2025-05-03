905dc8c7582c21776d95a75cce2176f597598436
Sreekarcheti <108572507+Sreekarcheti@users.noreply.github.com>
2025-05-03 19:39:10 +0530

    Update utils.py

--- style_transfer/utils.py
+++ style_transfer/utils.py
@@ -1,1 +1,16 @@
+import torch
+from torchvision import transforms
+from PIL import Image
 
+def load_image(img_path, size=None):
+    image = Image.open(img_path).convert('RGB')
+    if size:
+        image = image.resize((size, size), Image.LANCZOS)
+    transform = transforms.ToTensor()
+    image = transform(image).unsqueeze(0)
+    return image
+
+def save_image(tensor, path):
+    image = tensor.clone().detach().cpu().squeeze(0)
+    image = transforms.ToPILImage()(image)
+    image.save(path)
